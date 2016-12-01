edc35ba38618f45d074257b9a798eab087f340ce
MScevola <mario@scae.com.br>
2016-12-01 18:52:03 -0200

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,6 @@
+<form>
+  <input type="text" name="nome" placeholder="Nome" />
+  <input type="text" name="sobrenome" placeholder="Sobrenome" />
+  <input type="text" name="endereco" placeholder="Endereço" />
+  <input type="submit" name="salvar" value="Salvar" />
+</form>
