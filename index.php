e7f2ac2660d4179e954668cc915ceb8529a396bb
MScevola <mario@scae.com.br>
2016-12-02 17:54:36 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Crossknowledge - teste</title>
 
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
